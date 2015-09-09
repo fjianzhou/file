8d5608eee278e34bdce6d066a79555a5545d37f8
fjianzhou <fjianzhou@163.com>
2015-09-09 14:07:49 +0800

    添加文件

---
+++ server.js
@@ -0,0 +1,268 @@
+var http=require('http');
+var fs=require('fs');
+var path=require('path');
+var url= require('url');
+var mime=require('mime');
+
+http.createServer(function(req,res){
+    var rootPath=path.resolve();
+    var urls=url.parse(req.url);
+    console.log(urls.pathname);
+    var visitPath=path.join(rootPath,urls.pathname);
+
+
+    if(urls.pathname=='/')
+    {
+
+        fs.readFile('./index.html',function(err,data)
+        {
+            if(err)
+            {
+                res.setHeader("Content-Type","text/html;charset=utf-8");
+                res.end('文件读取失败！'+err);
+            }
+            else
+            {
+                res.end(data);
+            }
+
+        })
+    }
+    else if(urls.pathname=='/left.html')
+    {
+        fs.readdir(rootPath+'/Manager',function(err,files){
+            var htmlstr="";
+            if(files.length==0){return;}
+            files.forEach(function(currentFile){
+
+                var currentFilePath=path.join(rootPath,'Manager',currentFile).replace(/\\/g,"/");
+                fs.stat(currentFilePath,function(err,stata){
+
+                    var  temsrc=path.join("/",'Manager',currentFile).replace(/\\/g,"/");
+                    if(stata.isDirectory())
+                    {
+                        htmlstr+='<li class="dir">';
+                        htmlstr+='<a href ="javascript:onclick(\'left\',\''+temsrc+'\')">'+currentFile+'</a>'
+                        htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>'
+                        htmlstr+='<a href=""><img src="/imge/add.png"></a>'
+                        htmlstr+='</li>';
+                    }
+                    else if(stata.isFile())
+                    {
+                        var ext= path.extname(currentFile);
+                        switch (ext)
+                        {
+                            case '.jpg':
+                                htmlstr+='<li class="pic">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            case '.png':
+                                htmlstr+='<li class="pic">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            case '.jpeg':
+                                htmlstr+='<li class="pic">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            case '.txt':
+                                htmlstr+='<li class="file">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            case '.mp3':
+                                htmlstr+='<li class="mp3">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            case '.mp4':
+                                htmlstr+='<li class="video">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break;
+                            default :
+                                htmlstr+='<li class="file">';
+                                htmlstr+='<a href ="javascript:onclick(\'right\',\''+temsrc+'\')">'+currentFile+'</a>';
+                                htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                htmlstr+='</li>';
+                                break
+                        }
+                    }
+                })
+            })
+            fs.readFile('./left.html','utf-8',function(err,data){
+
+                res.setHeader('Content-Type',mime.lookup('./left.html'));
+
+                res.end(data.replace("#content",htmlstr));
+            })
+        })
+    }
+    else if(urls.pathname=='ajax')
+    {
+
+    }
+    else
+    {
+
+        fs.stat(visitPath,function(err,stat){
+            if(err)
+            {
+                res.setHeader("Content-Type","text/html;charset=utf-8");
+                res.end('未知异常！'+err);
+            }
+            else
+            {
+                if(stat.isFile())
+                {
+                    fs.readFile(visitPath,function(err,data)
+                    {
+                        if(err)
+                        {
+                            res.setHeader("Content-Type","text/html;charset=utf-8");
+                            res.end('文件读取失败！'+err);
+                        }
+                        else
+                        {
+
+                            if(path.extname(visitPath)=='.js')
+                            {
+                                res.setHeader('Content-Type',mime.lookup('./inedx.html'));
+                            }
+                            else
+                            {
+                                res.setHeader('Content-Type',mime.lookup(urls.pathname));
+                            }
+
+                            res.end(data);
+                        }
+
+                    })
+
+                }
+                else if(stat.isDirectory())
+                {
+                    fs.readdir(visitPath,function(err,files){
+                        var htmlstr="";
+                        if(files.length==0)
+                        {
+
+                            //var lastStrIndex= visitPath.lastIndexOf('/')
+                            //console.log(())
+                            //htmlstr+='<li>这个目录下没有发现文件';
+                            ////htmlstr+='<a href ="\''++'\'">'+返回上一级目录+'</a>';
+                            //htmlstr+='</li>';
+                        }
+                        files.forEach(function(currentFile){
+
+
+                            var currentFilePath=path.join(visitPath.replace(rootPath,""),currentFile).replace(/\\/g,"/");
+
+                            fs.stat(path.join(visitPath,currentFile),function(err,stata){
+
+                                if(err)
+                                {
+
+                                }
+                                if(stata.isDirectory())
+                                {
+
+                                    htmlstr+='<li class="dir">';
+                                    htmlstr+='<a href ="javascript:onclick(\'left\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                    htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                    htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                    htmlstr+='</li>';
+                                }
+                                else if(stata.isFile())
+                                {
+                                    var ext= path.extname(currentFile);
+
+                                    switch (ext)
+                                    {
+                                        case '.jpg':
+
+                                            htmlstr+='<li class="pic">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        case '.png':
+                                            htmlstr+='<li class="pic">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        case '.jpeg':
+                                            htmlstr+='<li class="pic">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        case '.txt':
+                                            htmlstr+='<li class="file">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        case '.mp3':
+                                            htmlstr+='<li class="mp3">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        case '.mp4':
+                                            htmlstr+='<li class="video">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                        default:
+                                            htmlstr+='<li class="file">';
+                                            htmlstr+='<a href ="javascript:onclick(\'right\',\''+currentFilePath+'\')">'+currentFile+'</a>';
+                                            htmlstr+='<a href ="" ><img src="/imge/delete.png" /></a>';
+                                            htmlstr+='<a href=""><img src="/imge/add.png"/></a>';
+                                            htmlstr+='</li>';
+                                            break;
+                                    }
+                                }
+                            })
+                        })
+                        fs.readFile('./left.html','utf-8',function(err,data){
+
+                            res.setHeader('Content-Type',mime.lookup('./left.html'));
+
+                            res.end(data.replace("#content",htmlstr));
+                        })
+                    })
+                }
+                else
+                {
+
+                }
+            }
+
+        })
+    }
+
+}).listen(8080);
---
+++ test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2015/9/7.
+ */
+currentFilePath='\\MyFileManager\\Manager\\3dir';
+currentFilePath='aaaaaa';
+currentFilePath=currentFilePath.replace('',"b","g")
+
+console.log(currentFilePath);
+
+var str = 'aaabbbcccaaabbbccc';
+var reg = /aaa/g;
+var res = str.replace(reg, '111');
+console.log(res);
